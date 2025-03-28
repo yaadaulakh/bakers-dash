8a6fa9fa8dec8392224689f50634edee94666d58
yaadaulakh <yaadkamal078aulakh@outlook.com>
2025-03-28 18:14:35 -0400

    makes some changes in custom post type

--- themes/no-code-theme/home.php
+++ themes/no-code-theme/home.php
@@ -1,32 +1,32 @@
 <?php
-// Custom query to fetch recipes
+// Query to fetch 6 latest recipes
 $args = array(
-    'post_type'      => 'recipe',
-    'posts_per_page' => 6, // Numbber of recipes to display
-    'orderby'        => 'date',
-    'order'          => 'DESC',
+    'post_type'      => 'recipe', // Use the "recipe" custom post type
+    'posts_per_page' => 3,        // Show 3 recipes
+    'orderby'        => 'date',   
+    'order'          => 'DESC',   
 );
 
-$query = new WP_Query($args);
+$query = new WP_Query($args); 
 
-if ($query->have_posts()) :
-    while ($query->have_posts()) : $query->the_post();
+if ($query->have_posts()) : 
+    while ($query->have_posts()) : $query->the_post(); 
         ?>
-        <div class="recipe-item">
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <div class="recipe-item"> <!-- Recipe container -->
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> 
             <?php if (has_post_thumbnail()) : ?>
                 <div class="recipe-thumbnail">
-                    <?php the_post_thumbnail('medium'); ?>
+                    <?php the_post_thumbnail('medium'); ?> 
                 </div>
             <?php endif; ?>
             <div class="recipe-excerpt">
-                <?php the_excerpt(); ?>
+                <?php the_excerpt(); ?> 
             </div>
         </div>
         <?php
     endwhile;
-    wp_reset_postdata(); // Reset the global post data
+    wp_reset_postdata(); 
 else :
-    echo '<p>No recipes found.</p>';
+    echo '<p>No recipes found.</p>'; 
 endif;
 ?>
